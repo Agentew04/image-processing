457bda16af309a28223f31bda685fba2297875ac
Rodrigo Appelt <rappelt@inf.ufsm.br>
2026-03-19 11:21:01 -0300

    1o commit

---
+++ Dataset/CaptureProject/Capture/Capture/CaptureHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using ABI.Windows.Graphics.Capture;
+
+namespace Capture;
+
+public class CaptureHelper {
+    public static GraphicsCaptureItem CreateItemForWindow(IntPtr hwnd)
+    {
+        var factory = WinRT.ActivationFactory.Get("Windows.Graphics.Capture.GraphicsCaptureItem");
+
+        var interop = factory.As<IGraphicsCaptureItemInterop>();
+
+        Guid iid = typeof(GraphicsCaptureItem).GUID;
+
+        interop.Vftbl.CreateForWindow(hwnd, ref iid, out object result);
+
+        return (GraphicsCaptureItem)result;
+    }
+}
---
+++ Dataset/CaptureProject/Capture/Capture/Capturer.cs
@@ -0,0 +1,7 @@
+﻿namespace Capture;
+
+public class Capturer {
+    public void Initialize() {
+        WinRT.ActivationFactory.Get()
+    }
+}
