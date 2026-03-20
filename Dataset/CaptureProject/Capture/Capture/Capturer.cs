f186ff5f421566a3454ddfde5363b56116825d2f
Rodrigo Appelt <rappelt@inf.ufsm.br>
2026-03-20 16:39:37 -0300

    feat: screen capture done

--- Dataset/CaptureProject/Capture/Capture/Capturer.cs
+++ Dataset/CaptureProject/Capture/Capture/Capturer.cs
@@ -1,7 +1,130 @@
-﻿namespace Capture;
+﻿using System.Runtime.InteropServices;
+using HPPH;
+using ScreenCapture.NET;
+using SkiaSharp;
 
-public class Capturer {
+namespace Capture;
+
+public class Capturer : IDisposable {
+    private DX11ScreenCaptureService screenCaptureService;
+    private List<Display> displays = [];
+    private Dictionary<IntPtr, Display> handleToDisplay = [];
+    private DX11ScreenCapture screenCapture;
+    private CaptureZone<ColorBGRA> captureZone;
+    private Task capturingTask;
+    private CancellationTokenSource cts;
+    private PeriodicTimer timer;
+
     public void Initialize() {
-        // WinRT.ActivationFactory.Get()
+        screenCaptureService = new DX11ScreenCaptureService();
+        IEnumerable<GraphicsCard> gpus = screenCaptureService.GetGraphicsCards();
+        GraphicsCard gpu = gpus.FirstOrDefault(x => x.Name.Contains("NVIDIA") || x.Name.Contains("AMD") && x.Name != "Microsoft Basic Render Driver");
+        if (gpu.Name == null) {
+            throw new Exception("Cant find suitable GPU");
+        }
+
+        displays.AddRange(screenCaptureService.GetDisplays(gpu));
     }
+
+    public void StartCapture(Window window, float targetFps) {
+        // find monitor of window 
+        IntPtr monitorHandle = MonitorFromWindow(window.Hwnd, MONITOR_DEFAULTTONEAREST);
+        if (!handleToDisplay.TryGetValue(monitorHandle, out Display display)) {
+            handleToDisplay.Clear();
+            EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, MonitorCallback, IntPtr.Zero);
+            display = handleToDisplay[monitorHandle];
+        }
+        
+        // se ainda eh null, deu algo mt errado
+        if (display.DeviceName == null) {
+            throw new Exception("Could not find display for the monitor that the selected window is at");
+        }
+        
+        screenCapture = screenCaptureService.GetScreenCapture(display);
+        captureZone = screenCapture.RegisterCaptureZone(0, 0, display.Width, display.Height);
+        
+        // spin up separate thread for capturing frames
+        cts = new CancellationTokenSource();
+        timer = new PeriodicTimer(TimeSpan.FromSeconds(1 / targetFps));
+        capturingTask = Task.Run(ThreadMain);
+    }
+
+    public void StopCapture() {
+        cts.Cancel();
+    }
+
+    private async Task ThreadMain() {
+        using SKBitmap bitmap = new();
+        SKImageInfo info = new(captureZone.Width, captureZone.Height, SKColorType.Bgra8888);
+        while (!cts.IsCancellationRequested && await timer.WaitForNextTickAsync(cts.Token)) {
+            screenCapture.CaptureScreen();
+            using (captureZone.Lock()) {
+                ref byte buffer = ref MemoryMarshal.GetReference(captureZone.RawBuffer);
+                unsafe {
+                    fixed (byte* ptr = &buffer) {
+                        bitmap.InstallPixels(info, (IntPtr)ptr);
+                    }
+                }
+                using SKData data = bitmap.Encode(SKEncodedImageFormat.Jpeg, 100);
+                string path = $"./img-{Random.Shared.Next()}.jpg";
+                await using FileStream fs = File.Create(path);
+                data.SaveTo(fs);
+                Console.WriteLine($"Captured screen to {path}");
+            }
+        }
+    }
+
+    public void Dispose() {
+        screenCaptureService.Dispose();
+    }
+
+    private bool MonitorCallback(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT rect, IntPtr data) {
+        MONITORINFOEX info = new();
+        info.cbSize = Marshal.SizeOf(info);
+        GetMonitorInfo(hMonitor, ref info);
+        string name = info.szDevice;
+        Display targetDisplay = displays.FirstOrDefault(x => x.DeviceName == name);
+        if (targetDisplay.DeviceName == null) {
+            Console.WriteLine("Could not map monitor to display. mapping to empty!");
+        }
+        handleToDisplay[hMonitor] = targetDisplay;
+        return true;
+    }
+
+    const uint MONITOR_DEFAULTTONEAREST = 0x00000002;
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr MonitorFromWindow(IntPtr hwnd, uint dwFlags);
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT {
+        public int left;
+        public int top;
+        public int right;
+        public int bottom;
+    }
+
+    public delegate bool MonitorEnumProc(
+        IntPtr hMonitor,
+        IntPtr hdcMonitor,
+        ref RECT lprcMonitor,
+        IntPtr dwData);
+
+    [DllImport("user32.dll")]
+    public static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumProc lpfnEnum, IntPtr dwData);
+    
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    public struct MONITORINFOEX
+    {
+        public int cbSize;
+        public RECT rcMonitor;
+        public RECT rcWork;
+        public uint dwFlags;
+
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+        public string szDevice;
+    }
+    
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    public static extern bool GetMonitorInfo(IntPtr hMonitor, ref MONITORINFOEX lpmi);
 }
--- Dataset/CaptureProject/Capture/Capture/Program.cs
+++ Dataset/CaptureProject/Capture/Capture/Program.cs
@@ -1,73 +1,31 @@
 ﻿using System.Runtime.InteropServices;
 using Windows.Graphics.DirectX.Direct3D11;
 using ABI.Windows.Graphics.Capture;
-using SharpGen.Runtime;
-using Vortice.Direct3D;
-using Vortice.Direct3D11;
-using Vortice.DXGI;
 using Windows.Foundation.Metadata;
+using HPPH;
+using ScreenCapture.NET;
+using SkiaSharp;
 
 namespace Capture;
 
 class Program {
     
     [STAThread]
-    static void Main(string[] args) {
-        List<Window> windows = WindowManager.GetOpenWindows();
-        foreach (Window window in windows) {
-            Console.WriteLine($"hWnd: {window.Hwnd}; Title: {window.Title}");
-        }
-        
-        return;
-        WinRT.ComWrappersSupport.InitializeComWrappers();
-        
-        Result result = D3D11.D3D11CreateDevice(
-            null,
-            DriverType.Hardware,
-            DeviceCreationFlags.BgraSupport,
-            null,
-            out ID3D11Device d3dDevice,
-            out ID3D11DeviceContext _);
-        if (result.Failure) {
-            Console.WriteLine("failed to get device");
-            return;
-        }
+    static async Task Main(string[] args) {
+        Capturer capturer = new();
+        capturer.Initialize();
 
-        Console.WriteLine("Got device");
-        
-        IDXGIDevice dxgiDevice = d3dDevice.QueryInterface<IDXGIDevice>();
-        IntPtr unknown;
-        result = D3DHelpers.CreateDirect3D11DeviceFromDXGIDevice(dxgiDevice.NativePointer, out unknown);
-        if (result.Failure) {
-            Console.WriteLine("Failed creating winrt device");
-            return;
+        List<Window> windows = WindowManager.GetOpenWindows();
+        Console.WriteLine("Selecione a janela para capturar");
+        for (int i = 0; i < windows.Count; i++) {
+            Console.WriteLine($"{i+1}. {windows[i].Title}");
         }
-        IDirect3DDevice winrtDevice =
-            WinRT.MarshalInterface<IDirect3DDevice>.FromAbi(unknown);
-        if (winrtDevice is null) {
-            Console.WriteLine("Failed to get WinRT device");
-            Marshal.Release(unknown);
-            return;
-        }
-        
-        bool supported =
-            ApiInformation.IsTypePresent("Windows.Graphics.Capture.GraphicsCaptureSession");
-        Console.WriteLine($"GraphicsCapture supported: {supported}");
-        if (!supported) {
-            Console.WriteLine("graphicscapture not supported");
-            return;
-        }
-        
-        IntPtr hwnd = D3DHelpers.GetForegroundWindow();
-        Console.WriteLine("Foreground HWND: " + hwnd);
-        
-        GraphicsCaptureItem item = CaptureHelper.CreateItemForWindow(hwnd);
-        // Console.WriteLine($"Capture size {item .Size.Width}x{item.Size.Height}");
-        
-        
-        
-        
-        
-        Marshal.Release(unknown);
+        Console.Write("> ");
+        string input = Console.ReadLine() ?? string.Empty;
+        int windowIndex = int.Parse(input)-1;
+        capturer.StartCapture(windows[windowIndex], 5);
+        await Task.Delay(1000);
+        capturer.StopCapture();
+        Console.WriteLine("Exit");
     }
 }
